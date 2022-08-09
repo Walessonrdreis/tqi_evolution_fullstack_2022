71a3d22853563a6f9273e960690bd85be79dc0c1
Walessonrdreis <wallllase@hotmail.com>
2022-08-08 22:17:11 -0300

    upadade: LivrariaService metodo:cadastrar

--- backend/livraria/src/main/java/br/com/api/livraria/services/LivrariaService.java
+++ backend/livraria/src/main/java/br/com/api/livraria/services/LivrariaService.java
@@ -1,8 +1,11 @@
 package br.com.api.livraria.services;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 
+import br.com.api.livraria.model.AnswerModel;
 import br.com.api.livraria.model.LivrariaModel;
 import br.com.api.livraria.repository.LivrariaRepository;
 
@@ -12,6 +15,11 @@ public class LivrariaService {
   @Autowired
   private LivrariaRepository lr;
 
+  @Autowired
+  private AnswerModel message;
+
+
+
   //metodo para listar todos os livros
  
   public Iterable<LivrariaModel> listar(){
@@ -19,4 +27,25 @@ public class LivrariaService {
     
   }
   
+  //Metodo para cadastrar livros
+  public ResponseEntity<?> cadastrar(LivrariaModel lm){
+    if(lm.getTitulo().equals("")){
+      message.setMensagem("O nome do titulo é obrigatório !");
+      return  new  ResponseEntity<AnswerModel>(message, HttpStatus.BAD_REQUEST);
+    }else if(lm.getAutor().equals("")){
+      message.setMensagem("O Nome do Autor é obrigatório!");
+      return new ResponseEntity<AnswerModel>(message, HttpStatus.BAD_REQUEST);
+    }else if(lm.getEditora().equals("")){
+      message.setMensagem("O Nome da Editora é obrigatório!");
+      return new ResponseEntity<AnswerModel>(message, HttpStatus.BAD_REQUEST);
+    } else if(lm.getImg().equals(null)){
+      message.setMensagem("A imagem do livro é Obrigatória!");
+      return new ResponseEntity<AnswerModel>(message, HttpStatus.BAD_REQUEST);
+    } else{
+      return new ResponseEntity<LivrariaModel>(lr.save(lm),HttpStatus.CREATED);
+    }
+    
+  }
+
+
 }
