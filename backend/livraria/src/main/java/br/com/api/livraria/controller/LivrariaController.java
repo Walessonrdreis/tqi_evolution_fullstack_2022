ae2071777cfe80321d76b597fa58f61572b33567
Walessonrdreis <wallllase@hotmail.com>
2022-08-08 21:11:24 -0300

    LivrariaController update

--- backend/livraria/src/main/java/br/com/api/livraria/controller/LivrariaController.java
+++ backend/livraria/src/main/java/br/com/api/livraria/controller/LivrariaController.java
@@ -1,5 +1,15 @@
 package br.com.api.livraria.controller;
 
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+
+@RestController
 public class LivrariaController {
+
   
+  @GetMapping("/")
+  public String rota(){
+    return "API de livros funcionando";
+  }
 }
