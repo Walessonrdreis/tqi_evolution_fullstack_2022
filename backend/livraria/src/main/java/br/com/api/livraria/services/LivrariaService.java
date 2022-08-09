697032ae59c6e742ad199282fdc8e5b524f4076f
Walessonrdreis <wallllase@hotmail.com>
2022-08-08 21:25:11 -0300

    LivariaService update

--- backend/livraria/src/main/java/br/com/api/livraria/services/LivrariaService.java
+++ backend/livraria/src/main/java/br/com/api/livraria/services/LivrariaService.java
@@ -1,5 +1,21 @@
 package br.com.api.livraria.services;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import br.com.api.livraria.model.LivrariaModel;
+
+@Service
 public class LivrariaService {
+
+  @Autowired
+  private LivrariaService ls;
+
+  //metodo para listar todos os livros
+
+  public Iterable<LivrariaModel> listar(){
+    return ls.findAll();
+    
+  }
   
 }
