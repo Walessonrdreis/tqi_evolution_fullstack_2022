bfc7f600f6628a8ca602f4c49ab5e73ab817f4b5
Walessonrdreis <wallllase@hotmail.com>
2022-08-09 17:58:09 -0300

    update: inserindo PostMaping(/cadastrar)

--- backend/livraria/src/main/java/br/com/api/livraria/controller/LivrariaController.java
+++ backend/livraria/src/main/java/br/com/api/livraria/controller/LivrariaController.java
@@ -1,7 +1,10 @@
 package br.com.api.livraria.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import br.com.api.livraria.model.LivrariaModel;
@@ -15,6 +18,11 @@ public class LivrariaController {
 
   @Autowired
   private LivrariaService ls;
+
+  @PostMapping("/cadastrar")
+  public ResponseEntity<?> cadastrar(@RequestBody LivrariaModel lm){
+      return ls.cadastrar(lm);
+  }
 
   @GetMapping("/listar")
   public Iterable<LivrariaModel> listar(){
