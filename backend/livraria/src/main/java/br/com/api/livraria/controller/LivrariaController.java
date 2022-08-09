c51e84412eb5b6d744cbbaef4f1aec1ef581772a
Walessonrdreis <wallllase@hotmail.com>
2022-08-08 21:44:21 -0300

    upadade: listar itens no SGB

--- backend/livraria/src/main/java/br/com/api/livraria/controller/LivrariaController.java
+++ backend/livraria/src/main/java/br/com/api/livraria/controller/LivrariaController.java
@@ -1,12 +1,25 @@
 package br.com.api.livraria.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import br.com.api.livraria.model.LivrariaModel;
+import br.com.api.livraria.services.LivrariaService;
 
 
 @RestController
 public class LivrariaController {
 
+
+
+  @Autowired
+  private LivrariaService ls;
+
+  @GetMapping("/listar")
+  public Iterable<LivrariaModel> listar(){
+    return ls.listar();
+  }
   
   @GetMapping("/")
   public String rota(){
