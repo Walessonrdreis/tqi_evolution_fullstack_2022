fd0d93d78f3b0b5169bd72d35c6b10f488573823
Walessonrdreis <wallllase@hotmail.com>
2022-08-09 21:44:53 -0300

    update: css/ bootstrap add useState

--- frontend/biblioteca_tequeience/src/pages/Signbooks/SignBooks.jsx
+++ frontend/biblioteca_tequeience/src/pages/Signbooks/SignBooks.jsx
@@ -1,13 +1,14 @@
-import React from 'react';
+import React,{useState} from 'react';
 import Form from '../../components/Form/Form';
 import Table from '../../components/Table/Table';
 
 
 const SignLivos = () => {
+  const [btnCadastrar, setbtnCadastrar] = useState(true);
   return (
     <div>
     <h1>Cadastro de Livros</h1>
-      <Form />
+      <Form button={btnCadastrar}/>
       <Table />
       
     </div>
