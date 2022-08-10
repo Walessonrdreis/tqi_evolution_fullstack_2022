0ffa0ed937605f31d95ee89d1a5cdb7bf110af8b
Walessonrdreis <wallllase@hotmail.com>
2022-08-09 21:30:27 -0300

    insert: bootstrap in forms

--- frontend/biblioteca_tequeience/src/components/Form/Form.jsx
+++ frontend/biblioteca_tequeience/src/components/Form/Form.jsx
@@ -4,15 +4,15 @@ import React from 'react'
 const Form = () => {
   return (
     <form>
-      <input type='text' placeholder='Título' />
-      <input type='text' placeholder='Autor' />
-      <input type='text' placeholder='Editora' />
+      <input type='text' placeholder='Título' className='form-control' />
+      <input type='text' placeholder='Autor' className='form-control' />
+      <input type='text' placeholder='Editora' className='form-control' />
       <label>Insira a imagem do livro</label>
-      <input type="file" accept="image/*"/>
-      <input type="button" value="Cadastrar"/>
-      <input type="button" value="Alterar"/>
-      <input type="button" value="Remover"/>
-      <input type="button" value="Cancelar"/>
+      <input type="file" accept="image/*" className='form-control'/>
+      <input type="button" value="Cadastrar" className='btn btn-primary'/>
+      <input type="button" value="Alterar" className='btn btn-warning'/>
+      <input type="button" value="Deletar" className='btn btn-danger'/>
+      <input type="button" value="Cancelar" className='btn btn-secondary'/>
       
     </form>
   )
