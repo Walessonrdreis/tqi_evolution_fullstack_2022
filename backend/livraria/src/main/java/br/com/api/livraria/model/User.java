5c96665ed5ad69fbb5ae2183b2a03c9b3cde7176
Walessonrdreis <wallllase@hotmail.com>
2022-08-22 21:26:47 -0300

    Criando tabela user.java, dados iniciais

--- backend/livraria/src/main/java/br/com/api/livraria/model/User.java
+++ backend/livraria/src/main/java/br/com/api/livraria/model/User.java
@@ -1,5 +1,37 @@
 package br.com.api.livraria.model;
 
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+@Table(name="usuarios")
+
 public class User {
-  
+
+  @Id
+  @GeneratedValue( strategy = GenerationType.IDENTITY)
+  private long id;
+
+  private String username;
+  private String password;
+  private String pais;
+  private String estado;
+  private String cidade;
+  private String endereco;
+  private String email;
+  private String telefone;
+  @Column(unique = true)
+  private String cpf;
+
 }
